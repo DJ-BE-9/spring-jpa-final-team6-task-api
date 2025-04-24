f68edeb310fcd4767a2e0fa6b953f2149f369cb8
김재혁 <rlawog@nate.com>
2025-04-24 09:58:58 +0900

    feat. 파일 추가2

--- src/main/java/com/nhnacademy/common/runner/AppStartupRunner.java
+++ src/main/java/com/nhnacademy/common/runner/AppStartupRunner.java
@@ -6,6 +6,5 @@ import org.springframework.boot.ApplicationRunner;
 public class AppStartupRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
     }
 }
