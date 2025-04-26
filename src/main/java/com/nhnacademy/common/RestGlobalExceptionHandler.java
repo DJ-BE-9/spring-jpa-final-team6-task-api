55ae4a7fa6e62ab351b2abc004caa095dd8a460f
김재혁 <rlawog@nate.com>
2025-04-26 16:29:47 +0900

    test

--- src/main/java/com/nhnacademy/common/RestGlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/common/RestGlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.nhnacademy.exception.ExceptionResponse;
 import com.nhnacademy.exception.MilestoneNotFoundException;
 import com.nhnacademy.exception.ProjectAlreadyExistsException;
 import com.nhnacademy.exception.ProjectNotFoundException;
+import com.nhnacademy.exception.comment.CommentNotFoundException;
 import com.nhnacademy.exception.projectTag.ProjectTagAlreadyExistsException;
 import com.nhnacademy.exception.projectTag.ProjectTagNotFoundException;
 import com.nhnacademy.exception.tag.TagAlreadyExistsException;
@@ -20,62 +21,30 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class RestGlobalExceptionHandler {
 
-    //project
-    @ExceptionHandler({ProjectAlreadyExistsException.class})
+    // AlreadyExistsException
+    @ExceptionHandler({
+            ProjectAlreadyExistsException.class,
+            TaskAlreadyExistsException.class,
+            TagAlreadyExistsException.class,
+            ProjectTagAlreadyExistsException.class,
+
+    })
     public ResponseEntity<ExceptionResponse> alreadyExistsException(ProjectAlreadyExistsException e) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.BAD_REQUEST.toString(),LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
     }
 
-    @ExceptionHandler({ProjectNotFoundException.class})
-    public ResponseEntity<ExceptionResponse> notFoundException(ProjectNotFoundException e) {
+    // NotFoundException
+    @ExceptionHandler({
+            ProjectNotFoundException.class,
+            TagNotFoundException.class,
+            TaskNotFoundException.class,
+            ProjectTagNotFoundException.class,
+            MilestoneNotFoundException.class,
+            CommentNotFoundException.class})
+    public ResponseEntity<ExceptionResponse> notFoundException(Exception e) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.NOT_FOUND.toString(),LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
-    }
-
-    //tag
-    @ExceptionHandler(TagAlreadyExistsException.class)
-    public ResponseEntity<ExceptionResponse> tagAlreadyExistsException(TagAlreadyExistsException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
-    }
-
-    @ExceptionHandler(TagNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> tagNotFoundException(TagNotFoundException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.NOT_FOUND.toString(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
-    }
-
-    //task
-    @ExceptionHandler(TaskAlreadyExistsException.class)
-    public ResponseEntity<ExceptionResponse> taskAlreadyExistsException(TaskAlreadyExistsException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
-    }
-
-    @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> taskNotFoundException(TaskNotFoundException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.NOT_FOUND.toString(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
-    }
-
-    //projectTag
-    @ExceptionHandler(ProjectTagAlreadyExistsException.class)
-    public ResponseEntity<ExceptionResponse> projectAlreadyExistsException(ProjectTagAlreadyExistsException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
-    }
-    @ExceptionHandler(ProjectTagNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> projectNotFoundException(ProjectTagNotFoundException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.NOT_FOUND.toString(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
-    }
-
-    //milestone
-    @ExceptionHandler(MilestoneNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> milestoneNotFoundException(MilestoneNotFoundException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(),HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
     }
 
     @ExceptionHandler(Exception.class)
